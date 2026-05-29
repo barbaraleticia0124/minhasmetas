7f0c33fa0e25083983c435657b42f3690716d15b
barbaraleticia0124 <barbara.dreher@escola.pr.gov.br>
2026-05-29 09:38:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const botoes= document.querySelectorALL(".botao");
+const textos= document.querySelectorAll(".aba-conteudo");
 
 for(leti=0;i <botoes.length;i++){
-    
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Data ("2023-10-05T00:00:00");
+let tempoAtual= new Data();
+
+contadores[0].textContent= tempoObjetivo1-tempoAtual;
