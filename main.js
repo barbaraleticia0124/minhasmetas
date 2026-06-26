31c11ee63084746a976959576d63b72fb471d9e9
barbaraleticia0124 <barbara.dreher@escola.pr.gov.br>
2026-06-26 09:37:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes= document.querySelectorALL(".botao");
 const textos= document.querySelectorAll(".aba-conteudo");
 
-for(leti=0;i <botoes.length;i++){
+for(let i=0;i < botoes.length;i++){
     botoes[i].onclick = function(){
         
         for(let j=0;j<botoes.length;j++){
@@ -17,29 +17,29 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Data ("2024-10-05T00:00:00");
 const tempoObjetivo2 = new Data ("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Data ("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Data ("2025-01-01T00:00:00");
+const tempoObjetivo4 = new Data ("2025-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo (tempoObjetivo){
     let tempoAtual= new Data();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/ 1000);
     let minutos = Math.floor(segundos/ 60);
-    let horas = Math.floor (minutos/ 60);
-    let dias = Math.floor (horas/ 24);
+    let horas = Math.floor(minutos/ 60);
+    let dias = Math.floor(horas/ 24);
 
     segundos %=60;
     minutos %=60;
     horas %=24;
     if (tempoFinal >0){
-        return [dias, horas, minutos, segundos];
+        return [dias,horas,minutos,segundos];
     } else{
         return[0,0,0,0];
     }
 }
 
-function atuqalizaCronometro(){
+function atualizaCronometro(){
     for (let i=0; i <contadores.length;i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
          document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
@@ -48,7 +48,7 @@ function atuqalizaCronometro(){
     }
 }
 
-function comecacronometro(){
+function comecaCronometro(){
     atualizaCronometro(); 
     setInterval(atualizaCronometro, 1000);
 }
